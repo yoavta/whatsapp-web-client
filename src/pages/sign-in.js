d14337d67d8a3ad009b597e4cf64d3de58c906e7
yoavtamirp <tamiryoav@gmail.com>
2022-03-30 12:59:21 +0300

    work JSON hardcoded file

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,6 +1,7 @@
-
+import ServiceServer from '../server-service';
 
 function SignIn() {
+    ServiceServer.print_users();
     return (
       <div className="Sign-in">
         <p>sign in page</p>
---
+++ src/server-service.js
@@ -0,0 +1,11 @@
+import jsonContent from './assets/hard-coded-users.json';
+const ServiceServer = {
+
+    print_users: function() {
+
+        jsonContent.Users
+
+    }
+}
+
+export default ServiceServer;
