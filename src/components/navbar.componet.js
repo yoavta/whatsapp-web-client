5e829f6de94a51d09da889bd8dea1f9779c8a8a7
YahelJac <yahel020@gmail.com>
2022-03-30 12:11:30 +0300

    added hard-coded-data

--- src/components/navbar.componet.js
+++ src/components/navbar.componet.js
@@ -8,8 +8,8 @@ function NavbarMain() {
             <Container>
                 <Navbar.Brand href="#home">Home</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/signin">Sign-In</Nav.Link>
-                    <Nav.Link href="#features">Registertion</Nav.Link>
+                    <Nav.Link href="/signin">Sign-In</Nav.Link>
+                    <Nav.Link href="/register">Registertion</Nav.Link>
                    
                 </Nav>
             </Container>
