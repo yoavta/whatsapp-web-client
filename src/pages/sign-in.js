260d45f7c9ed0b3399a52d81e4d617b3705da160
YahelJac <yahel020@gmail.com>
2022-04-02 20:23:14 +0300

    try to pull

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,10 +1,12 @@
 import ServiceServer from '../server-service';
+import Users from './assets/hard-coded-users.json';
 
 function SignIn() {
     ServiceServer.print_users();
     return (
       <div className="Sign-in">
         <p>sign in page</p>
+        <p>Users.nickname</p>
       </div>
     );
   }
