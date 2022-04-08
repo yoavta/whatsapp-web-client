c7d06ddfc54c33c4635afd83d6f330b65915c129
yoavtamirp <tamiryoav@gmail.com>
2022-04-08 14:44:10 +0300

    adding option for larger image in chats

--- src/components/displayMsg.js
+++ src/components/displayMsg.js
@@ -1,25 +1,69 @@
-import { Button } from 'bootstrap';
-import massage from '../components/massage'
-import {Image} from "react-bootstrap";
-import React from "react";
-import ServiceServer from "../server-service";
+import {Col, Image, Modal, Row} from 'react-bootstrap';
+import React, {useState} from "react";
 
 
 function DisplayMsg({massage, style}) {
-    if (massage.mediaType==='image'){
-       const  chats = ServiceServer.getUsers('yoav')
+    const [fullscreen, setFullscreen] = useState(true);
+    const [show, setShow] = useState(false);
+
+
+    const handleClose = () => {
+        setShow(false);
+        console.log('close')
     }
-    return (   
-        
+    const handleShow = () => {
+        setShow(true);
+        console.log('show')
+    }
+
+
+    return (
+
         <div className="card" style={style}>
             <div className="card-body" style={{position: 'relative', textAlign: 'left'}}>
 
-                <h6 className="card-subtitle mb-2 text-muted" >{massage.date}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{massage.date}</h6>
+                {massage.mediaType === "text" && <p className="card-text">{massage.text}</p>}
 
-                {massage.mediaType == "text" && <p className="card-text" >{massage.text}</p>}
-                {massage.mediaType == "image" && <Image style={{maxHeight: '100px', maxWidth: '100px'}} className={"center-block"} src={massage.media}/>}
-                {massage.mediaType == "video" && <video style={{maxHeight: '200px', maxWidth: '200px'}} controls className={"center-block"} src={massage.media}/>}
+
+                {massage.mediaType === "video" &&
+                    <video style={{maxHeight: '200px', maxWidth: '200px'}} controls className={"center-block"}
+                           src={massage.media}/>}
+                <div is={'button'} style={{border: 'none', background: 'none', margin: 'none', cursor: 'zoom-in'}}
+                     onClick={() => handleShow()}>
+
+                    {massage.mediaType === "image" &&
+                        <Image style={{maxHeight: '100px', maxWidth: '100px'}} className={"center-block"}
+                               src={massage.media}/>}
+
+
+                </div>
             </div>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Preview</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row className="justify-content-md-center">
+                        <Col md={"auto"}>
+                            {massage.mediaType === "image" &&
+                                <Image className={"center-block"}
+                                       src={massage.media}/>}
+                        </Col>
+                    </Row>
+
+                </Modal.Body>
+
+            </Modal>
         </div>
+
+
     );
-} export default DisplayMsg;
+}
+
+export default DisplayMsg;
