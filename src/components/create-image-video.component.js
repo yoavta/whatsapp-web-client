0b553b44f1274d6d05e4b5618c9a286db360d3ab
yoavtamirp <tamiryoav@gmail.com>
2022-04-11 20:31:01 +0300

    start image capture component

---
+++ src/components/create-image-video.component.js
@@ -0,0 +1,45 @@
+import {Col, Container, Form, Image, Row} from "react-bootstrap";
+import React ,{useState, useEffect} from "react";
+
+function SendImageVideo(props) {
+
+    let typeForAccept = props.type.concat("/*");
+    console.log(typeForAccept);
+
+    useEffect(() => {
+
+        props.mediaPrev != null && props.mediaChanged()},[props.mediaPrev])
+
+    return (<Container>
+            <Form.Group className="mb-3" controlId="formImage">
+                <Form.Label>Choose {props.type}</Form.Label>
+                <Form.Control required type="file" accept={typeForAccept}
+                              onChange={event => {
+
+                                  const val = URL.createObjectURL(event.target.files[0]);
+                                  props.setMediaPrev(val);
+
+
+                                  // props.mediaChanged();
+                              }}
+                />
+            </Form.Group>
+
+            <Row className="justify-content-md-center">
+                <Col md={"auto"}>
+                    {props.type === 'image' && (props.mediaPrev != null) &&
+                        <Image style={{width: "100px", height: "100px"}} className={"center-block"} src={props.mediaPrev}/>}
+                    {props.type === 'video' && (props.mediaPrev != null) &&
+                        <video width="400" controls className={"center-block"} src={props.mediaPrev}/>}
+                </Col>
+            </Row>
+
+        </Container>
+
+
+    );
+
+
+}
+
+export default SendImageVideo;
